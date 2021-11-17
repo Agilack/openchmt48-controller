bb7173236575aec6478c84ea4a2eb280113a7de0
Florent Jousseaume <florent@agilack.fr>
2021-11-17 10:50:24 +0100

    Drop STM32Cube code, create bare-metal firmware skeleton

---
+++ firmware/src/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file  main.c
+ * @brief Main function of the firmware
+ *
+ * @author Saint-Genest Gwenael <gwen@agilack.fr>
+ * @copyright Agilack (c) 2021
+ *
+ * @page License
+ * This firmware is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation. You should have received a
+ * copy of the GNU Lesser General Public License along with this program,
+ * see LICENSE.md file for more details.
+ * This program is distributed WITHOUT ANY WARRANTY.
+ */
+
+/**
+ * @brief Entry point of the firmware
+ *
+ * @return integer Exit code (but should never returns)
+ */
+int main(void)
+{
+	while(1);
+}
+/* EOF */
